3df149ccdfba0ca746de58ecf3dd4e5ddbaaacb8
undesputed <inyong89@gmail.com>
2015-07-09 05:51:21 +0000

    more robust? connection logic, adding the sending of ACK

--- TargetClient/targetClientToNode.py
+++ TargetClient/targetClientToNode.py
@@ -14,24 +14,31 @@ DEVICE_ASK_TYPE = "areYouDevice"
 
 socketIO = None
 
+# class OSLoggingNamespace(LoggingNamespace):
+#
+#   def on_disconnect(self):
+#     super(LoggingNamespace,self).on_disconnect()
+#     init()
+
+
 def on_request(*args):
-  print 'Open request received. Executing request and sending response '
+  print ('Open request received. Executing request and sending response ',args)
   logging.info('Open request received. Executing request and sending response ')
-  socketIO.emit(ACK_TYPE)
+  socketIO.emit(ACK_TYPE,args)
   os.system("echo 5=165 > /dev/servoblaster")
   time.sleep(5)
   os.system("echo 5=100 > /dev/servoblaster")
 
 def on_misc(*args):
-    print('Misc Message Received: ', args)
-    # getting an ErrorType/string formatting error.
-    # Figure out args structure later.
-    # logging.info("Misc Message Received: '{0}'", args[0])
+  print('Misc Message Received: ', args)
+  # getting an ErrorType/string formatting error.
+  # Figure out args structure later.
+  # logging.info("Misc Message Received: '{0}'", args[0])
 
 def on_server_start(*args):
-    print('Server Startup Message Received: ', args)
-    socketIO.wait(seconds=1)
-    socketIO.emit(STARTUP_TYPE, 'frontDoor')
+  print('Server Startup Message Received: ', args)
+  socketIO.wait(seconds=1)
+  socketIO.emit(STARTUP_TYPE, 'frontDoor')
 
 def init():
   global socketIO
@@ -41,8 +48,6 @@ def init():
   socketIO.on(REQUEST_TYPE, on_request)
   socketIO.on(MISC_MSG_TYPE, on_misc)
   socketIO.on(DEVICE_ASK_TYPE, on_server_start)
-  # socketIO.wait(seconds=1)
-  # socketIO.emit(STARTUP_TYPE, 'frontDoor')
   socketIO.wait()
 
 if __name__ == "__main__":
